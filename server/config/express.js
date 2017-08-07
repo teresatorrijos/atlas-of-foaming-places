ec47a6360b6029b2252ee291fc4d69bb24ee6e28
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-08-07 18:49:52 +0200

    It was my fault. sorry

--- server/config/express.js
+++ server/config/express.js
@@ -37,7 +37,8 @@ module.exports = function(app){
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
+    // app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static('public'));
     app.use(layouts);
 
     app.use(session({
